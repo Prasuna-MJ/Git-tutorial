c4fb84419af0483c8a68665b6b10e15eee0021a8
prasuna <mjprasuna07@gmail.com>
2023-11-16 15:01:00 +0530

    wished added

--- javaprograms/Hello_World.java
+++ javaprograms/Hello_World.java
@@ -1,7 +1,7 @@
 class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("How are you");
+        System.out.println("How are you GOOD MORNING");
 
     }
 
